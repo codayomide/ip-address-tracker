1292f2fd70d5660a5ebb464c68b4ad491d3c4c29
codayomide <smokayomide@gmail.com>
2023-11-15 17:25:11 +0100

    Added custom screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         veryDarkGray: "hsl(0, 0%, 17%)",
-        darkGray: "hsl(0, 0%, 59%)"
-      }
+        darkGray: "hsl(0, 0%, 59%)",
+        neutralWhite: "hsl(0, 0%, 100%)",
+      },
+      screens: {
+        "mo-xs": "0px",
+        "mo-sm": "350px",
+        "mo-md": "475px",
+        "mo-lg": "576px",
+      },
+    },
+    backgroundImage: {
+      "image-mobile-bg": "url('src/assets/images/pattern-bg-mobile.png')",
+      "image-desktop-bg": "url('src/assets/images/pattern-bg-desktop.png')",
     },
   },
   plugins: [],
-}
+};
