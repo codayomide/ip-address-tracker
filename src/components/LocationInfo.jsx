5b347a543d2a70fe89befb8b184e5a0a12b6939f
codayomide <smokayomide@gmail.com>
2023-11-15 17:26:00 +0100

    Commit on 'main'

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -1,22 +1,22 @@
 const LocationInfo = () => {
   return (
-    <div>
-      <div>
+    <div className="bg-neutralWhite text-center py-5 sm:py-8 shadow-lg w-[80%] sm:w-[70%] rounded-lg mt-4 sm:mt-12 sm:px-6 sm:text-left sm:flex sm:justify-between">
+      <div className="location-info">
         <h4>IP Address</h4>
         <h3>192.212.174.101</h3>
       </div>
 
-      <div>
+      <div className="location-info">
         <h4>Location</h4>
         <h3>Brooklyn, NY 10001</h3>
       </div>
 
-      <div>
+      <div className="location-info">
         <h4>Timezone</h4>
         <h3>UTC -05:00</h3>
       </div>
 
-      <div>
+      <div className="location-info">
         <h4>ISP</h4>
         <h3>SpaceX Starlink</h3>
       </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,12 @@
 const SearchBar = () => {
   return (  
-    <form>
-      <input type="text" />
-      <button>
-        <img src="images/icon-arrow.svg" alt="" />
+    <form className="flex text-lg mt-4 w-[45%]">
+      <input type="text" placeholder="Search for any IP address or domain" className="px-5 py-3 rounded-l-xl w-full"/>
+      <button className="bg-veryDarkGray rounded-r-xl px-5 py-3">
+        <img src="src/assets/images/icon-arrow.svg" alt="" />
       </button>
     </form>
-  );
+  )
 }
  
 export default SearchBar;
