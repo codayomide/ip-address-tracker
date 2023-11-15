53ee4fd30c4495359d31e3b3fe76bc7a3efa8f21
codayomide <smokayomide@gmail.com>
2023-11-15 17:25:44 +0100

    Created the basic css layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import LocationInfo from "./components/LocationInfo";
+import Map from "./components/Map";
 import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-    <main className="app">
-      <section>
-        <h1>IP Address Tracker</h1>
+    <main className="app h-screen w-full">
+      <section className="bg-image-desktop-bg h-[40%] sm:h-[30%] flex flex-col items-center pt-8">
+        <h1 className="text-neutralWhite font-medium text-2xl">IP Address Tracker</h1>
 
         <SearchBar />
 
         <LocationInfo />
       </section>
 
-      
+      <Map />
     </main>
   );
 }
