312ec712b706a5f5c04eecec98462b2a2d964b3b
codayomide <smokayomide@gmail.com>
2023-11-15 01:30:52 +0100

    Added custom colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        veryDarkGray: "hsl(0, 0%, 17%)",
+        darkGray: "hsl(0, 0%, 59%)"
+      }
+    },
   },
   plugins: [],
 }
