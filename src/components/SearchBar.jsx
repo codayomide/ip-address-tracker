917011dfb4ba2f4cdbe1ea08718378d5bfc248e9
codayomide <smokayomide@gmail.com>
2023-11-15 17:42:22 +0100

    Worked on search bar responsiveness

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 const SearchBar = () => {
   return (  
-    <form className="flex text-lg mt-4 w-[45%]">
+    <form className="flex text-sm sm:text-lg mt-4 mo-md:mt-8 w-[95%] mo-sm:w-[90%] mo-md:w-[80%] sm:w-[60%] md:w-[50%] lg:w-[45%]">
       <input type="text" placeholder="Search for any IP address or domain" className="px-5 py-3 rounded-l-xl w-full"/>
       <button className="bg-veryDarkGray rounded-r-xl px-5 py-3">
         <img src="src/assets/images/icon-arrow.svg" alt="" />
