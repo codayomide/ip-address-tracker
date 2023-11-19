875f462505008f98848ee8ddd290891341be1669
codayomide <smokayomide@gmail.com>
2023-11-19 17:44:19 +0100

    worked on style

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -1,22 +1,22 @@
 const LocationInfo = () => {
   return (
-    <div className="bg-neutralWhite text-center py-5 sm:py-8 shadow-lg w-[80%] sm:w-[70%] rounded-lg mt-4 sm:mt-12 sm:px-6 sm:text-left sm:flex sm:justify-between">
+    <div className="bg-neutralWhite text-center py-5 md:py-6 shadow-lg w-[80%] mo-lg:w-[65%] sm:w-[55%] md:w-[85%] xl:w-[75%] rounded-lg mt-4 md:mt-12 md:px-6 md:text-left md:flex md:justify-between">
       <div className="location-info">
         <h4>IP Address</h4>
         <h3>192.212.174.101</h3>
       </div>
 
-      <div className="location-info">
+      <div className="location-info left-border">
         <h4>Location</h4>
         <h3>Brooklyn, NY 10001</h3>
       </div>
 
-      <div className="location-info">
+      <div className="location-info left-border">
         <h4>Timezone</h4>
         <h3>UTC -05:00</h3>
       </div>
 
-      <div className="location-info">
+      <div className="location-info left-border">
         <h4>ISP</h4>
         <h3>SpaceX Starlink</h3>
       </div>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,8 @@
 const SearchBar = () => {
   return (  
     <form className="flex text-sm sm:text-lg mt-4 mo-md:mt-8 w-[95%] mo-sm:w-[90%] mo-md:w-[80%] sm:w-[60%] md:w-[50%] lg:w-[45%]">
-      <input type="text" placeholder="Search for any IP address or domain" className="px-5 py-3 rounded-l-xl w-full placeholder:invisible mo-sm:placeholder:visible"/>
-      <button className="bg-veryDarkGray rounded-r-xl px-5 py-3">
+      <input type="text" placeholder="Search for any IP address or domain" className="px-5 py-3 rounded-l-xl w-full placeholder:invisible mo-sm:placeholder:visible focus:outline-none outline-none"/>
+      <button className="bg-veryDarkGray hover:bg-darkGray transition-colors ease-linear duration-75 rounded-r-xl px-5 py-3">
         <img src="src/assets/images/icon-arrow.svg" alt="" />
       </button>
     </form>
