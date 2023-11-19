5af1c2fa9bd3b3454be88e090cc5dc7b7b75c74c
codayomide <smokayomide@gmail.com>
2023-11-19 11:34:04 +0100

    Worked on responsiveness for search bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SearchBar from "./components/SearchBar";
 function App() {
   return (
     <main className="app h-screen w-full">
-      <section className="bg-image-desktop-bg h-[40%] sm:h-[30%] flex flex-col items-center pt-8">
+      <section className="bg-image-desktop-bg bg-cover h-[40%] sm:h-[30%] flex flex-col items-center pt-8">
         <h1 className="text-neutralWhite font-medium text-2xl">IP Address Tracker</h1>
 
         <SearchBar />
